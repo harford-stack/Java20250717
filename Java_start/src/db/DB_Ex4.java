2eeebab4244c4bb0e205653d1240a44a827bfb0e
TJ-BU-708-009PC <TJ-BU-708-009PC@DESKTOP-QQ3AMHG>
2025-08-13 18:25:02 +0900

    2025.08.13

---
+++ Java_start/src/db/DB_Ex4.java
@@ -0,0 +1,48 @@
+package db;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class DB_Ex4 {
+
+	public static void main(String[] args) throws SQLException {
+		
+		// STUDENT
+		// 학번을 입력 받아서 해당 학번의 학생이 있으면
+		// 해당 학생의 키를 1 증가 후 '키를 1 증가했습니다' 출력
+		// 없으면 '학생이 없습니다' 출력
+		
+		Scanner s = new Scanner(System.in);
+		System.out.print("학번 입력 : ");
+		String stuNo = s.next();
+		
+		String sql = "UPDATE STUDENT SET STU_HEIGHT = STU_HEIGHT + 1 WHERE STU_NO = '" + stuNo + "'";
+		
+		DBClass db = new DBClass();
+		int num = db.stmt.executeUpdate(sql);
+		if(num > 0) {
+			System.out.println("키를 1 증가했습니다");
+		} else {
+			System.out.println("학생이 없습니다");
+		}
+		
+		
+//		String sql = "SELECT * FROM STUDENT";
+//		ResultSet rs = db.stmt.executeQuery(sql);
+//		
+//		String sql1 = "UPDATE STUDENT SET STU_HEIGHT = STU_HEIGHT + 1 WHERE STU_NO = " + stuNo;
+//		int rs1 = db.stmt.executeUpdate(sql1);
+//		
+//		Scanner s = new Scanner(System.in);
+//		
+//		while(rs.next()) {
+//			String stuNo = s.next();
+//			if(stuNo.equals(rs.getString("STU_NO"))) {
+//				
+//			}
+//		}
+
+	}
+
+}
--- Java_start/src/module-info.java
+++ Java_start/src/module-info.java
@@ -6,4 +6,5 @@
  */
 module Java_start {
 	requires java.desktop;
+	requires java.sql;
 }
